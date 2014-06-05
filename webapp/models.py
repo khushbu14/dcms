edec64781ddc3d76bb69ed920c3c919b6691a510
psachin <iclcoolster@gmail.com>
2014-06-05 11:19:52 +0530

    Subject: New models.
    Description:
    
    - This is much cleaner approach.
    - Any number of classes & subjects can be added.

--- webapp/models.py
+++ webapp/models.py
@@ -2,176 +2,83 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Contributer(models.Model):
-  
+class Contributor(models.Model):
+    """Contributor."""
     user = models.OneToOneField(User)
 
     # Addition info
     contact = models.CharField(max_length=12, blank=True)
-    picture = models.ImageField(upload_to='profile_image', blank=True)
+    picture = models.ImageField(upload_to='contributor_profile_image', blank=True)
 
     def __unicode__(self):
         return self.user.username
+
 
 class Reviewer(models.Model):
-  
+    """Reviewer."""
     user = models.OneToOneField(User)
 
     # Addition info
     contact = models.CharField(max_length=12, blank=True)
-    picture = models.ImageField(upload_to='profile_image', blank=True)
+    picture = models.ImageField(upload_to='reviewer_profile_image', blank=True)
 
     def __unicode__(self):
         return self.user.username
 
-class standard_9_mathematics(models.Model):
+
+class Class(models.Model):
+    """This will be class number like first, second .. eight .. tenth."""
+    class_number = models.IntegerField(default=1)
+    remark = models.TextField()
+
+    def __unicode__(self):
+        return u"%d" % (self.class_number)
 
 
-    """class:9 subject: Mathematcis """
-    topic_name = models.CharField(max_length=200, unique=True)
-    pdf_doc = models.FileField(upload_to='docs', blank=True)
-    video_doc = models.FileField(upload_to='docs', blank=True)
+class Subject(models.Model):
+    """Subjects."""
+    name = models.CharField(max_length=200, unique=True)
+    class_number = models.ForeignKey(Class)
+    pdf = models.FileField(upload_to='docs', blank=True)
+    video = models.FileField(upload_to='videos', blank=True)
     url = models.URLField(blank=True)
-    date_upload = models.DateField(auto_now=True)
-    contributer = models.ForeignKey(Contributer) 
+    uploaded_on = models.DateField(auto_now=True)
+    contributor = models.ForeignKey(Contributor) 
     download_count = models.IntegerField(default=0)
     rating = models.IntegerField(default=0)
     approve = models.BooleanField(default=False)
 
+
     def __unicode__(self):
-        return self.topic_name        
+        return self.name
+
 
     def increment_download_count(self):
         self.download_count += 1
         self.save()
 
-
-class standard_9_physics(models.Model):
-
-
-    """class:9 subject: Physics """
-    topic_name = models.CharField(max_length=200, unique=True)
-    pdf_doc = models.FileField(upload_to='docs', blank=True)
-    video_doc = models.FileField(upload_to='docs', blank=True)
-    url = models.URLField(blank=True)
-    date_upload = models.DateField(auto_now=True)
-    contributer = models.ForeignKey(Contributer) 
-    download_count = models.IntegerField(default=0)
-    rating = models.IntegerField(default=0)
-    approve = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.topic_name        
-
-    def increment_download_count(self):
-        self.download_count += 1
+    
+    def increment_rating(self):
+        self.rating += 1
         self.save()
 
-
-
-class standard_9_chemistry(models.Model):
-
-
-    """class:9 subject: chemistry """
-    topic_name = models.CharField(max_length=200, unique=True)
-    pdf_doc = models.FileField(upload_to='docs', blank=True)
-    video_doc = models.FileField(upload_to='docs', blank=True)
-    url = models.URLField(blank=True)
-    date_upload = models.DateField(auto_now=True)
-    contributer = models.ForeignKey(Contributer) 
-    download_count = models.IntegerField(default=0)
-    rating = models.IntegerField(default=0)
-    approve = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.topic_name        
-
-    def increment_download_count(self):
-        self.download_count += 1
-        self.save()
-
-
-class standard_10_mathematics(models.Model):
-
-
-    """class:10 subject: Mathematcis """
-    topic_name = models.CharField(max_length=200, unique=True)
-    pdf_doc = models.FileField(upload_to='docs', blank=True)
-    video_doc = models.FileField(upload_to='docs', blank=True)
-    url = models.URLField(blank=True)
-    date_upload = models.DateField(auto_now=True)
-    contributer = models.ForeignKey(Contributer) 
-    download_count = models.IntegerField(default=0)
-    rating = models.IntegerField(default=0)
-    approve = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.topic_name        
-
-    def increment_download_count(self):
-        self.download_count += 1
-        self.save()
-
-
-
-class standard_10_physics(models.Model):
-
-
-    """class:10 subject: physics """
-    topic_name = models.CharField(max_length=200, unique=True)
-    pdf_doc = models.FileField(upload_to='docs', blank=True)
-    video_doc = models.FileField(upload_to='docs', blank=True)
-    url = models.URLField(blank=True)
-    date_upload = models.DateField(auto_now=True)
-    contributer = models.ForeignKey(Contributer) 
-    download_count = models.IntegerField(default=0)
-    rating = models.IntegerField(default=0)
-    approve = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.topic_name        
-
-    def increment_download_count(self):
-        self.download_count += 1
-        self.save()
-
-class standard_10_chemistry(models.Model):
-
-
-    """class:10 subject: chemistry """
-    topic_name = models.CharField(max_length=200, unique=True)
-    pdf_doc = models.FileField(upload_to='docs', blank=True)
-    video_doc = models.FileField(upload_to='docs', blank=True)
-    url = models.URLField(blank=True)
-    date_upload = models.DateField(auto_now=True)
-    contributer = models.ForeignKey(Contributer) 
-    download_count = models.IntegerField(default=0)
-    rating = models.IntegerField(default=0)
-    approve = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.topic_name        
-
-    def increment_download_count(self):
-        self.download_count += 1
-        self.save()
-
-
-
-
+        
 class Contact(models.Model):
-    """ Contacts """
+    """Contact us."""
     name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
     message = models.TextField(max_length=500)
 
+
     def __unicode__(self):
-        return self.email
+        return u"%s - %s" % (self.name, self.email)
+
 
 class Faq(models.Model):
     """FAQs"""
     question = models.TextField(max_length=500)
-    answer = models.TextField(max_length=1000)
+    answer = models.TextField() #
+
 
     def __unicode__(self):
         return self.question
